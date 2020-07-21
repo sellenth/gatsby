50ff325424fea038b3ae1b7df370cb955b08485e
sellenth <sellenth@oregonstate.edu>
2020-07-21 11:39:38 -0700

    .

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default function Contact() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Link to="/">Home</Link>
+      <p>Send us a message!</p>
+    </div>
+  );
+}
